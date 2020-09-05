969d6926bd4c8d7ef130085db6a3d3b9503e5d15
smadrom <sn.one.dev@gmail.com>
2020-09-05 22:20:13 +0300

    1.0.0

---
+++ _output/extension_hint.php
@@ -0,0 +1,9 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
+
+namespace SM\EV\XF\Validator
+{
+	class XFCP_Email extends \XF\Validator\Email {}
+}
